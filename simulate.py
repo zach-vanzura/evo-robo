99cb3fe8f67969c4864b98afe8eb5effb06702a4
zach-vanzura <zvanzura@uvm.edu>
2025-01-16 13:28:59 -0500

    began implementation of sim

--- simulate.py
+++ simulate.py
@@ -1,2 +1,15 @@
 import pybullet as p
+import time
 
+physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)  # set to 0 to enable
+
+for i in range(1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(1 / 60)
+
+
+
+
+p.disconnect()
