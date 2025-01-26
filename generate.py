790d621754bb4ee8a5075c42e4d268fad0e0e600
zach-vanzura <zvanzura@uvm.edu>
2025-01-26 14:14:38 -0500

    Created multiple boxes, and functionality to have a 10 high 5x5 "pyramid" of boxes

--- generate.py
+++ generate.py
@@ -1,8 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
+length, width, height = 1, 1, 1
+x, y, z = 0, 0, 0.5
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+for x_ in range(5):
+    z = 0.5
+    length, width, height = 1, 1, 1
+    for y_ in range(5):
+        z = 0.5
+        length, width, height = 1, 1, 1
+        for z_ in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x + x_, y + y_, z], size=[length, width, height])
+            length, width, height = length * 0.9, width * 0.9, height * 0.9
+            z += height + height * 0.05  # 0.05 is half of the decrease in height
 
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)  # set to 0 to enable
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     print(i)
