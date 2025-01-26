998a56a58c822db1a84efb21b693bb2bd4593e74
zach-vanzura <zvanzura@uvm.edu>
2025-01-26 12:19:29 -0500

    Added a floor to the world

--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)  # set to 0 to enable
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 
 for i in range(1000):
     print(i)
